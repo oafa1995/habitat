4a390301daef4c9a36bf7c9fde0afc64601b0bbe
adonay <oafa1995@hotmail.com>
2021-09-14 16:17:44 -0600

    make it better

--- docs/perfil.php
+++ docs/perfil.php
@@ -89,7 +89,7 @@ while ($fila = pg_fetch_array($query_s)) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
   <link rel="icon" href="../img/tittle.ico">
   <script src="../js/sweet-alert.min.js"></script>
