789e0cd70b9c67b56493120aa05a8915ecde447d
adonay <oafa1995@hotmail.com>
2021-09-27 17:14:02 -0600

    make it better

--- config/conexion.php
+++ config/conexion.php
@@ -1,6 +1,6 @@
 <?php
 $server="ec2-52-23-40-80.compute-1.amazonaws.com";
-$puerto="5432";
+$puerto="54320";
 $database="dfug7mqfvt0m0t";
 $usuario="lrabxoraylrbte";
 $clave="4fb97b294515e022a9b3b3bb736f5b349a35d3a742d0dc8d5581ae6e0ce95d61";
